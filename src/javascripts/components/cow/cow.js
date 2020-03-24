7e8d0c65e69a392c9a013b858dc138337e8b9d18
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-03-23 19:23:19 -0500

    got cows to print to dom

---
+++ src/javascripts/components/cow/cow.js
@@ -0,0 +1,16 @@
+const cowMaker = (cow) => {
+  let domString = '';
+  domString += '<div class="col-3">';
+  domString += '<div class="card">';
+  domString += `<div class="card-header">${cow.name}</div>`;
+  domString += '<div class="card-body">';
+  domString += `<h5 class="card-title">${cow.breed}</h5>`;
+  domString += `<p class="card-text">Weight: ${cow.location}</p>`;
+  domString += `<p class="card-text">Weight: ${cow.weight}</p>`;
+  domString += '</div>';
+  domString += '</div>';
+  domString += '</div>';
+  return domString;
+};
+
+export default { cowMaker };
--- src/javascripts/components/pasture/pasture.js
+++ src/javascripts/components/pasture/pasture.js
@@ -1,8 +1,19 @@
 import cowData from '../../helpers/data/cowData';
+import util from '../../helpers/util';
+import cowComponent from '../cow/cow';
 
 const buildCows = () => {
   cowData.getCows()
-    .then((response) => console.error('getCows worked', response.data))
+    .then((cows) => {
+      let domString = '';
+      domString += '<h2 class="text-center">Pasture</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      cows.forEach((cow) => {
+        domString += cowComponent.cowMaker(cow);
+      });
+      domString += '</div>';
+      util.printToDom('pasture', domString);
+    })
     .catch((err) => console.error('getCows broke', err));
 };
 
